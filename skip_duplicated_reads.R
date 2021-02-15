2efdac5d674f4ce0fdeb04dfee82bb61f693ad4a
Maxim-Ivanov <35961244+Maxim-Ivanov@users.noreply.github.com>
2021-02-15 18:56:21 +0100

    Add files via upload

--- skip_duplicated_reads.R
+++ skip_duplicated_reads.R
@@ -1,5 +1,13 @@
 skip_duplicated_reads <- function(bam) {
   nms <- names(bam)
+  if (is.null(nms)) {
+    stop("GAlignments object must have read names!")
+  }
+  message(length(bam), " input BAM records;")
   idx <- which(duplicated(nms) | duplicated(nms, fromLast = TRUE))
-  return(bam[-idx])
+  if (length(idx) > 0) {
+    message("Skipped ", length(idx), " BAM records with duplicated names (", length(unique(nms[idx])), " chimeric reads);")
+    bam <- bam[-idx]
+  }
+  return(bam)
 }
